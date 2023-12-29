dda3d64cb5daa18723718f92850b504da3af354c
Alfredo <amorganr@gmail.com>
2023-12-29 12:24:19 -0300

    Update index.js
    Agrego una segunda linea con los bracket <code></code> preguntandome si lo que me falta es hacer un push para que se reflejen estos contenidos

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
     <br> Esta linea es insertada desde un browser en el editor de github</br>
+    Ya hice un <code>commit</code> pero creo que me falta un <code>push</code>, que no he sabido encontrar si es que hay que hacerlo
         </p>
       </main>
 
